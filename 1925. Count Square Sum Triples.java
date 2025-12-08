1084cb4b460ec03f1e4b8d53f8390f12ababbf67
Shubham Singh <120304300+shubhsingh1515@users.noreply.github.com>
2025-12-08 23:42:00 +0530

    Implement countTriples method to count square sum triples

---
+++ 1925. Count Square Sum Triples.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countTriples(int n) {
+        int cnt=0;
+
+        for(int a=1;a<n;a++){
+            for(int b=1;b<n;b++){
+                int c_sq = a*a + b*b;
+                int c = (int)Math.sqrt(c_sq);
+                if(c*c == c_sq && c<=n)
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
